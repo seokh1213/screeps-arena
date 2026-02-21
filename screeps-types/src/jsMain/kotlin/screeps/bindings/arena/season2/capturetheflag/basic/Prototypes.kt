3d6eed7d6a65de301f28d7092bb53ed193ddab7b
홍석현 <seokh1213@gmail.com>
2026-02-21 21:31:54 +0900

    chore: season2 - capture the flag 신규 타입 추가

---
+++ screeps-types/src/jsMain/kotlin/screeps/bindings/arena/season2/capturetheflag/basic/Prototypes.kt
@@ -0,0 +1,17 @@
+@file:JsModule("arena/season_2/capture_the_flag/basic/prototypes")
+@file:JsNonModule
+
+package screeps.bindings.arena.season2.capturetheflag.basic
+
+import screeps.bindings.BodyPartConstant
+import screeps.bindings.arena.GameObject
+
+typealias BodyPartType = BodyPartConstant
+
+/**
+ * A separate part of creep body
+ */
+abstract external class BodyPart : GameObject {
+    /** The type of the body part */
+    val type: BodyPartType
+}
---
+++ typings-source/season_2/capture_the_flag/basic.d.ts
@@ -0,0 +1,4 @@
+declare module "arena/season_2/capture_the_flag/basic" {
+    export * from "arena/season_2/capture_the_flag/basic/prototypes";
+    export * from "arena/season_2/capture_the_flag/basic/constants";
+}
